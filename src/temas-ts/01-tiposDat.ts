dda11bf642f9c3f1dd11a2d4f3154ddc44045e66
Alberto Isaac <ptisaac10@gmail.com>
2024-09-06 18:54:10 -0600

    Commit de clase 06 de septiembre

---
+++ src/temas-ts/01-tiposDat.ts
@@ -0,0 +1,20 @@
+var num1=38
+var num2=29
+const num3=10
+
+let sum=num1+num2+num3;
+
+let nombre:string
+let num:number
+let activo:boolean
+
+nombre='Axel'
+num=11
+activo=true
+
+let matricula:number|string|boolean
+
+matricula=20000993
+matricula='A134'
+
+console.log('El numero de la suma es:',sum);
---
+++ src/temas-ts/02-interfaz.ts
@@ -0,0 +1,13 @@
+interface Alumno{
+    nombre:string;
+    edad:number;
+    email:string;
+    calificacion?:number;
+}
+
+const alumno:Alumno={ //aqui esta el objeto aqui se crea una instancia
+    nombre:'Alberto',
+    edad:21,
+    email: 'ptisa@gmail.com'
+
+}
